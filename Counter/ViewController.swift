5976a665a9877d628c084e8bb425f4b085a441f7
t-kulikova <tatsiana.kulikova@yandex.ru>
2023-03-22 01:16:11 +0300

    Created IBOutlets and IBActions

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
 
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var historyChangesTextView: UITextView!
+    
+    // MARK: - UIViewController Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - IBActions
+    
+    @IBAction private func resetCounter(_ sender: Any) {
+        // TODO: - Implement recetCounter
+    }
+    
+    @IBAction private func incrementCounter(_ sender: Any) {
+        // TODO: - Implement incrementCounter
+    }
+    
+    @IBAction private func decrementCounter(_ sender: Any) {
+        // TODO: - Implement decrementCounter
+    }
+    
 }
-
